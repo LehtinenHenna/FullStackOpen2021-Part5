0f191cdd7b38deb254f608410a50e6c8643ce720
henna <henna.lehtinen91@gmail.com>
2022-02-28 15:49:17 +0200

    Did task 5.7: Made it possible for one blog to have two views, a more and less detailed view. Improved the appearance with some inline CSS

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -115,7 +115,7 @@ const App = () => {
           </Togglable>
         </div>
         {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+          <Blog key={blog.id} blog={blog}/>
         )}
       </div>
     )
--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({blog}) => {
+  const [fullView, setFullView] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+    <div style={blogStyle}>
+      {fullView 
+      ? <div>
+          <p>
+            {blog.title}&nbsp;
+            <button onClick={ () => setFullView(!fullView) }>{ fullView? 'hide' : 'show' }</button>
+          </p>
+          <p>{blog.author}</p>
+          <p>
+            likes {blog.likes}&nbsp;
+            <button>like</button>
+          </p>
+          <p>{blog.url}</p>
+        </div>
+
+      : <div>
+          <p>
+            {blog.title}&nbsp;
+            {blog.author? 'by ' : ''}
+            {blog.author}&nbsp;
+            <button onClick={ () => setFullView(!fullView) }>{ fullView? 'hide' : 'show' }</button>
+          </p>
+        </div>
+      }
+    </div>  
+  )
+}
 
 export default Blog
